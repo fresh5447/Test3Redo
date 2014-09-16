ffb37c45bfc40895ab7dd20667c0467fed3cff4d
Doug Walter <doug@kosmojo.com>
2014-09-15 21:22:13 -0700

    working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var grabAvatar = process.env.GRAB_AVATAR;
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', grabAvatar: grabAvatar });
 });
 
 module.exports = router;
